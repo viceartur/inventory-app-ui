c8440e586319a6163584e2e6e2e835c0bad8315d
Artur <capone56j@gmail.com>
2024-12-12 12:25:53 -0500

    Removed states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
 
 export default function MainPage() {
-  const [serverStatus, setServerStatus] = useState("Loading...");
-  const [dbStatus, setDBstatus] = useState("Loading...");
-
   return (
     <section>
       <h2>Welcome to the Inventory Management App v2.0</h2>
