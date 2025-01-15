3e62eb9728f7a348e39e023c58227fe84f5fc7fa
Artur <capone56j@gmail.com>
2025-01-15 16:52:55 -0500

    upd styles

--- src/components/nav-links.tsx
+++ src/components/nav-links.tsx
@@ -23,47 +23,47 @@ export function NavLinks() {
 
   return (
     <nav>
-      <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
+      <Link className={`link${pathname === "/" ? " active" : ""}`} href="/">
         Main Page
       </Link>
       <Link
-        className={`link ${pathname === "/customer" ? "active" : ""}`}
+        className={`link${pathname === "/customer" ? " active" : ""}`}
         href="/customer"
       >
         Customers
       </Link>
       <Link
-        className={`link ${pathname === "/warehouse" ? "active" : ""}`}
+        className={`link${pathname === "/warehouse" ? " active" : ""}`}
         href="/warehouse"
       >
         Warehouse & Locations
       </Link>
       <Link
-        className={`link ${pathname === "/send-material" ? "active" : ""}`}
+        className={`link${pathname === "/send-material" ? " active" : ""}`}
         href="/send-material"
       >
         Send Material
       </Link>
       <Link
-        className={`link ${pathname === "/incoming-materials" ? "active" : ""}`}
+        className={`link${pathname === "/incoming-materials" ? " active" : ""}`}
         href="/incoming-materials"
       >
         Incoming Materials {quantity ? "(" + quantity + ")" : ""}
       </Link>
       <Link
-        className={`link ${pathname === "/materials" ? "active" : ""}`}
+        className={`link${pathname === "/materials" ? " active" : ""}`}
         href="/materials"
       >
         Inventory
       </Link>
       <Link
-        className={`link ${pathname === "/reports" ? "active" : ""}`}
+        className={`link${pathname === "/reports" ? " active" : ""}`}
         href="/reports"
       >
         Reports
       </Link>
       <Link
-        className={`link ${pathname === "/import_data" ? "active" : ""}`}
+        className={`link${pathname === "/import_data" ? " active" : ""}`}
         href="/import_data"
       >
         Import Materials
